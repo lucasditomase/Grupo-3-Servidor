2b6d8c32f3aa9993c0dfe395a93085f6678081ae
Tomas Cortina <tomascortina00@uca.edu.ar>
2024-10-13 23:45:01 -0300

    Added App.js which uses express API to comunicate with the server

--- script.ts
+++ script.ts
@@ -2,23 +2,11 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
+
 async function main() {
     await prisma.usuario.deleteMany()
-    const usuario = await prisma.usuario.createMany({
-        data: [
-        {
-            nombre: "Joe Hendry",
-            email: "joehendry@gmail.com",
-            edad: 25
-        },
-        {
-            nombre: "DUMMYEAH",
-            email: "DUMMYEAH@gmail.com",
-            edad: 25
-        }
-    ]})
-    console.log(usuario)
 }
+
 
 main()
     .catch(
